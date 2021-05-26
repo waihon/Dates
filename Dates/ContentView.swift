6018c97517897eb71ec1409e720eba8e38f0d967
Waihon Yew <yewwaihon@gmail.com>
2021-05-26 18:17:14 +0800

    Date components

--- Dates/ContentView.swift
+++ Dates/ContentView.swift
@@ -13,13 +13,20 @@ struct ContentView: View {
     components.hour = 7
     components.minute = 30
     let dateFromComponents = Calendar.current.date(from: components) ?? Date()
-        
+    
+    let currentDateTime = Date()
+    let dateComponents = Calendar.current.dateComponents([.hour, .minute], from: currentDateTime)
+
     return Form {
       Section(header: Text("Date from Components")) {
         Text("Hour: \(components.hour ?? 0)")
         Text("Minute: \(components.minute ?? 0)")
         Text("Date: \(dateFromComponents)")
 
+      }
+      Section(header: Text("Date Components")) {
+        Text("Hour: \(dateComponents.hour ?? 0)")
+        Text("Minute: \(dateComponents.minute ?? 0)")
       }
     }
   }
